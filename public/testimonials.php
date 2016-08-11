4ef51b9a29e8bd848c4f4b424de9fda5d4e0db57
Benjamin Silveston <bsilveston37@gmail.com>
2016-08-11 15:39:51 +0100

    Testimonials page - first commit

---
+++ public/testimonials.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Testimonials | Hitchin Coffee Lab</title>
+	<link rel="stylesheet" type="text/css" href="css/testimonials.css">
+
+	<!-- Site Icon -->
+	<link rel="icon" type="image/jpeg" href="images/logo.jpg" />
+</head>
+<body>
+	<div class="container">
+		<?php include 'views/header.php'; ?>
+		<div class="jumbotron">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Testimonials</h1>
+					<h4>See what others have been saying about us!</h4>
+					<p>Key points and accolades:</p>
+					<ul>
+						<li>3rd best place in Hitchin to drink coffee</li>
+						<li>Average 4.7 rating (out of 5 stars)</li>
+						<li>Cakes, lattes and flat-whites among the most popular products</li>
+					</ul>
+				</div>					
+			</div>
+			<h2>Best Reviews</h2>
+			<p>Check out our best reviews below and more on <a href="https://www.facebook.com/Hitchin-Coffee-LAB-548073462007766/reviews" target="_blank">Facebook</a> and <a href="https://www.tripadvisor.co.uk/Restaurant_Review-g658254-d10547149-Reviews-Hitchin_Coffee_Lab-Hitchin_Hertfordshire_England.html" target="_blank">Tripadvisor</a>!</p>
+			<div class="row">
+				<div class="col-sm-6">				
+					<blockquote class="quote">Lovely spot with excellent coffee, so my boyfriend tells me. I just come for the cake and little lemony things that look like baby croissants? Yep. Don't eat one or you'll never stop and there's loads of flavours. Cute place, friendly staff. Recommend.<br /><br />
+					<span> - Claire Humphries</span></blockquote>
+				</div>
+				<div class="col-sm-6">
+					<blockquote class="quote">The most important thing about Hitchin Coffee Lab is this - the coffee is fantastic. In addition, the staff are lovely; welcoming us and our tiny baby, helping us with the pram, and generally being lovely and nice. We can't wait to go back.<br /><br />
+					<span> - Dave Guinane</span></blockquote>
+				</div>
+			</div> <!-- End of first .row -->
+			<div class="row">
+				<div class="col-sm-6">				
+					<blockquote class="quote">Since the first time I visited I have craved their chai latte... I now make up any excuse to go and get one!! Only slight disappointment is that they're not big enough!!!.<br /><br />
+					<span> - Jane Ballard</span></blockquote>
+				</div>
+				<div class="col-sm-6">
+					<blockquote class="quote">Oh my god - the cakes were delicious - as was the coffee ! My 12 yr old declared her latte the best ever - high praise indeed from a young coffee connoisseur !<br /><br />
+					<span> - Chris Jones</span></blockquote>
+				</div>
+			</div> <!-- End of second .row -->
+			<div class="row">
+				<div class="col-sm-6">				
+					<blockquote class="quote">Great coffee, laid back atmosphere and very cool venue. I have found my new favourite spot for flat whites and remote working in Hitchin :-)<br /><br />
+					<span> - Matt Hollingdale</span></blockquote>
+				</div>
+				<div class="col-sm-6">
+					<blockquote class="quote">Very friendly, excellent coffee - keep drooling over the cakes (not literally, mind)...haven't tried one yet but they always look amazing!<br /><br />
+					<span> - Anne Cowley</span></blockquote>
+				</div>
+			</div> <!-- End of third .row -->						
+		</div><!-- End of .jumbotron -->		
+		<?php include 'views/footer.php'; ?>
+	</div><!-- End of .container -->
+</body>
+</html>
