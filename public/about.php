b69aae744ad73958c567fa44c1cbfa12f2def6a2
Benjamin Silveston <bsilveston37@gmail.com>
2016-08-11 15:41:32 +0100

    About page - first commit

---
+++ public/about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About | Hitchin Coffee Lab</title>
+	<link rel="stylesheet" type="text/css" href="css/about.css" />
+	<!-- Site Icon -->
+	<link rel="icon" type="image/jpeg" href="images/logo.jpg" />
+</head>
+<body>
+	<div class="container">
+		<?php include 'views/header.php'; ?>
+		<div class="jumbotron">
+			<div class="row">
+				<div class="col-sm-6">
+					<h1>About Us</h1>
+					<p>Hitchin Coffee Lab started in January 2016 and since it's inception, we have seen many satisfied customers!</p> 
+					<h2>What "the science behind hot drinks" really means</h2>
+					<p>We are a friendly, passionate and dedicated to one thing - coffee (oh and cake too)! Through extensive (scientific) research, we find the best blend of coffee to whet your appetite and satisfy those taste-buds.</p>
+					<p>We take huge pride in providing a high-quality service for all of our customers! When you enter Hitchin Coffee Lab, you will be greeted and treated like no other coffee shop before!</p>
+					<p>See for yourself! Pop-in for a cuppa anytime!</p>					
+				</div>
+				<div class="col-sm-6">
+					<img id="founders-pic-1" src="images/hitchin_coffee_lab_staff.jpg" /><br />
+					<img id="founders-pic-2" src="images/family.jpg" />
+				</div>
+			</div>
+		</div><!-- End of .jumbotron -->
+		<?php include 'views/footer.php'; ?>
+	</div><!-- End of .container -->
+</body>
+</html>
