c5760bb8f8ba93fe7637cd75c8cc02c522a59ec8
Benjamin Silveston <bsilveston37@gmail.com>
2016-08-11 15:46:13 +0100

    Gallery page - first commit

---
+++ public/gallery.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gallery | Hitchin Coffee Lab</title>
+	<link rel="stylesheet" type="text/css" href="css/gallery.css" />
+
+	<!-- Site Icon -->
+	<link rel="icon" type="image/jpeg" href="images/logo.jpg" />
+</head>
+<body>
+	<div class="container">
+		<?php include 'views/header.php'; ?>
+		<div class="jumbotron">
+			<div class="row">
+				<div class="col-sm-12">
+					<h1>Gallery</h1>
+					<h4>Check out our photos and see what Hitchin Coffee Lab really is about!</h4>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="photo" id="photo-1">
+						<a href="#" target="_blank">
+							<img src="images/two-coffees(2).jpg" />
+						</a> 
+						<div class="photo-description">Photo description</div>
+					</div>	
+				</div><!-- End of first photo -->
+				<div class="col-sm-4">
+					<div class="photo" id="photo-2">
+						<a href="#" target="_blank">
+							<img src="images/interior_2.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>
+				</div><!-- End of second photo -->
+				<div class="col-sm-4">
+					<div class="photo" id="photo-3">
+						<a href="#" target="_blank">
+							<img src="images/donut.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>
+				</div> <!-- End of third photo -->
+			</div><!-- End of first .row -->
+			<br />
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="photo" id="photo-4">
+						<a href="#" target="_blank">
+							<img src="images/cakes.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>	
+				</div><!-- End of fourth photo -->
+				<div class="col-sm-4">
+					<div class="photo" id="photo-5">
+						<a href="#" target="_blank">
+							<img src="images/blending.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>
+				</div><!-- End of fifth photo -->
+				<div class="col-sm-4">
+					<div class="photo" id="photo-6">
+						<a href="#" target="_blank">
+							<img src="images/pictures.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>
+				</div> <!-- End of sixth photo -->
+			</div><!-- End of second .row -->
+			<br />
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="photo" id="photo-7">
+						<a href="#" target="_blank">
+							<img src="images/lobby.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>	
+				</div><!-- End of seventh photo -->
+				<div class="col-sm-4">
+					<div class="photo" id="photo-8">
+						<a href="#" target="_blank">
+							<img src="images/training.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>
+				</div><!-- End of eigth photo -->
+				<div class="col-sm-4">
+					<div class="photo" id="photo-9">
+						<a href="#" target="_blank">
+							<img src="images/art.jpg" />
+						</a>
+						<div class="photo-description">Photo description</div>
+					</div>
+				</div> <!-- End of ninth photo -->
+			</div><!-- End of first .row -->					
+		</div><!-- End of .jumbotron -->
+		<?php include 'views/footer.php'; ?>
+	</div><!--End of .container -->
+</body>
+</html>
