9345cf76e0ecd7a14c1f0a404d5b1f0bd625e5a7
Benjamin Silveston <bsilveston37@gmail.com>
2016-08-11 15:43:19 +0100

    Updated index page - changed photo and added welcome message.

--- public/index.php
+++ public/index.php
@@ -2,26 +2,27 @@
 <html>
 <head>
 	<title>Home | Hitchin Coffee Lab</title>
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link rel="stylesheet" type="text/css" href="css/index.css" />
+
+	<!-- Site Icon -->
+	<link rel="icon" type="image/jpeg" href="images/logo.jpg" />
 </head>
 <body>
-<?php 
-
-// Include header
-include 'views/header.php';
-?>
-
-<div class="jumbotron">
-	<div class="row">
-		<div class="col-sm-6">
-			<h1>Welcome!</h1>
-		</div>
-	</div>
-</div><!-- End of .jumbotron -->
-
-<?php 
-// Include footer
-include 'views/footer.php';
-?>
+	<div class="container">
+		<?php include 'views/header.php'; ?>
+		<div class="jumbotron">
+			<div class="row">
+				<div class="col-sm-6">
+					<h1>Welcome!</h1>
+					<p>You've reached the site of Hitchin Coffee Lab! Scientifically the best coffee shop in Hitchin (in our minds)!</p>
+					<p>Please do feel free to look around to find out more about the place, where science meets coffee!</p>
+				</div>
+				<div class="col-sm-6">
+					<img id="coffee-lab-exterior" src="images/exterior.jpg" />
+				</div>
+			</div>
+		</div><!-- End of .jumbotron -->
+		<?php include 'views/footer.php'; ?>
+	</div><!-- End of .container -->
 </body>
 </html>
