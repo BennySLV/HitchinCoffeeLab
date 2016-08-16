fd01f80f97b5f69c6d3f95762258f080617eef18
Benjamin Silveston <bsilveston37@gmail.com>
2016-08-16 17:32:39 +0100

    Added contact page form front-end button functionality and form validation.

--- public/contact.php
+++ public/contact.php
@@ -2,9 +2,20 @@
 <html>
 <head>
 	<title>Contact | Hitchin Coffee Lab</title>
-	<link rel="stylesheet" type="text/css" href="css/contact.css">
+	<!-- Accompanying stylesheet -->
+	<link rel="stylesheet" type="text/css" href="css/contact.css" />
+
 	<!-- Site Icon -->
 	<link rel="icon" type="image/jpeg" href="images/logo.jpg" />
+
+	<!-- jQuery library CDN -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+
+	<!-- jQuery-UI library CDN -->
+	<script src="https://code.jquery.com/ui/1.12.0-rc.2/jquery-ui.js"></script>
+
+	<!-- Accompanying JavaScript -->
+	<script type="text/javascript" src="js/contact.js"></script>
 </head>
 <body>
 	<div class="container">
@@ -22,7 +33,7 @@
 				</div>
 				<div class="col-sm-6">
 					<h3>Opening Hours</h3>
-					<table>
+					<table id="opening-hours">
 						<tr>
 							<th>Day</th>
 							<th>Hours</th>
@@ -38,20 +49,22 @@
 					</table><br />
 					<button id="contact-form-button">Alternatively, please click here to use the contact form</button>
 					<form id="contact-form" action="contact.php" method="post" autocomplete="off">
-						<fieldset><legend id="form-instructions">Please fill in all fields<button id="close-button">X Close</button></legend>						
-							<p><input id="first-name" type="text" name="first-name" placeholder="First Name" /> <span id="first-name-flag"><i aria-hidden="true"></i></span> <span id="first-name-validation"></span></p>
+						<fieldset><legend id="form-instructions">Please fill in all fields</legend>			
+							<p><input id="first-name" type="text" name="first_name" placeholder="First Name" /> <span id="first-name-flag"><i aria-hidden="true"></i></span> <span id="first-name-validation"></span></p>
 							<p><input id="surname" type="text" name="surname" placeholder="Surname" /> <span id="surname-flag"><i aria-hidden="true"></i></span> <span id="surname-validation"></span></p>
-							<p><input id="email" type="email" name="email" placeholder="Email-Address" /> <span id="email-flag"><i aria-hidden="true"></i></span> <span id="email-validation"></span></p>
-							<p><textarea id="message" placeholder="Message..." rows="5" cols="25"></textarea> <span id="message-flag"><i aria-hidden="true"></i></span> <span id="message-validation"></span></p>
-							<input id="reset-button" type="button" name="resetButton" value="Reset" />
+							<p><input id="email" type="email" name="email" placeholder="Email" /> <span id="email-flag"><i aria-hidden="true"></i></span> <span id="email-validation"></span></p>
+							<p><input id="confirm-email" type="email" name="confirm_email" placeholder="Confirm Email" /> <span id="confirm-email-flag"><i aria-hidden="true"></i></span> <span id="confirm-email-validation"></span></p>
+							<p><textarea id="message" placeholder="Message..." rows="5" cols="19"></textarea> <span id="message-flag"><i aria-hidden="true"></i></span> <span id="message-validation"></span></p>
+							<button id="reset-button" name="resetButton" value="Reset">Reset</button><br /><br />
 							<input id="send-button" type="submit" name="submit" value="Send" />
+							<button id="form-close-button">X Close</button>
 						</fieldset>
 					</form>					
 				</div><!-- End of .row -->
 			</div><br />
 			<div class="row">
 				<div class="col-sm-12">					
-					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2459.299933201359!2d-0.2809358488796533!3d51.94672177961019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487633fa726f2bc5%3A0x8f1075b73a7da4eb!2s29+Sun+St%2C+Hitchin+SG5+1AH!5e0!3m2!1sen!2suk!4v1470666923273" width="1020" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+					<iframe id="map-directions" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2459.299933201359!2d-0.2809358488796533!3d51.94672177961019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487633fa726f2bc5%3A0x8f1075b73a7da4eb!2s29+Sun+St%2C+Hitchin+SG5+1AH!5e0!3m2!1sen!2suk!4v1470666923273" width="1020" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 			</div>
 		</div><!-- End of .jumbotron -->
---
+++ public/js/contact.js
@@ -0,0 +1,240 @@
+/** 
+* Contact Form JavaScript/jQuery
+*
+* This code is designed to be re-used for all Front-End 
+* contact-form validation
+*
+* @author Benjamin Silveston <bsilveston37@gmail.com> 
+*/
+
+// Shorthand for $(document).ready(function({ });
+$(function() {
+	// Initially hide the contact form when the page loads
+	$('#contact-form').hide();
+
+	// Show the form when the user clicks the button
+	$('#contact-form-button').on('click', function(event) {
+		event.preventDefault(); // Prevent the entire page from reloading
+
+		// Show the inner contact form
+		$('#contact-form').fadeIn('slow');
+	});
+
+	// First Name Regular Expression
+	var $firstNameRegex = new RegExp(/^[a-zA-Z -]{3,16}$/);
+
+	// First name text input
+	$('#first-name').on('blur', function() {
+		// First name text input value
+		var $firstNameInput = $(this).val();
+
+		/* If the first name input text-box is NOT empty AND if the first name is valid 
+			(i.e. contains the necessary characters) then insert the "green tick" indicator
+		*/
+		if($.trim($firstNameInput).length) {
+			if($firstNameRegex.test($firstNameInput)) {
+				$('#first-name-flag').removeClass('fa fa-times').addClass('fa fa-check');
+				$('#first-name-validation').removeClass('validation-fail').addClass('validation-pass').text(' First name is valid!');	
+			}
+			else { // If the first name contains illegal characters
+				$('#first-name-flag').removeClass('fa fa-check').addClass('fa fa-times');
+				$('#first-name-validation').removeClass('validation-pass').addClass('validation-fail').text(' First name is not valid!');
+			}			
+		}
+		// If the input text-box is empty then insert the "red X" indicator
+		else {
+			$('#first-name-flag').removeClass('fa fa-check').addClass('fa fa-times');
+			$('#first-name-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter first name!');						
+		}			
+	});
+
+	// Surname Regular Expression
+	var $surnameRegex = new RegExp(/^[a-zA-Z -]{3,16}$/);
+	
+	// Surname text input
+	$('#surname').on('blur', function() {
+		// Surname text input value
+		var $surnameInput = $(this).val();
+
+		/* If the surname input text-box is NOT empty AND if the surname is valid 
+			(i.e. contains the necessary characters) then insert the "green tick" indicator
+		*/
+		if($.trim($surnameInput).length) {
+			if($surnameRegex.test($surnameInput)) {
+				$('#surname-flag').removeClass('fa fa-times').addClass('fa fa-check');
+				$('#surname-validation').removeClass('validation-fail').addClass('validation-pass').text(' Surname is valid!');	
+			}
+			else { // If the surname contains illegal characters
+				$('#surname-flag').removeClass('fa fa-check').addClass('fa fa-times');
+				$('#surname-validation').removeClass('validation-pass').addClass('validation-fail').text(' Surname is not valid!');
+			}			
+		}
+		// If the input text-box is empty then insert the "red X" indicator
+		else {
+			$('#surname-flag').removeClass('fa fa-check').addClass('fa fa-times');
+			$('#surname-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter surname!');						
+		}
+	});
+
+	// Email address Regular Expression
+	var $emailRegex = new RegExp(/^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,4}$/);	
+
+	// Email Address input field
+	$('#email').on('blur', function() {	
+
+		// Email address input value
+		var $emailInput = $(this).val();
+
+		/* If the email input text-box is NOT empty 
+			- AND if the email is valid
+			(i.e. contains the necessary characters ('@', '.com,.org,etc')) then insert the "green tick" indicator
+		*/		
+		if($.trim($emailInput).length) {
+			if($emailRegex.test($emailInput)) {
+				$('#email-flag').removeClass('fa fa-times').addClass('fa fa-check');
+				$('#email-validation').removeClass('validation-fail').addClass('validation-pass').text(' Email address is valid!');												
+			}
+			else { // If the email address DOES NOT contain necessary regex characters
+				$('#email-flag').removeClass('fa fa-check').addClass('fa fa-times');
+				$('#email-validation').removeClass('validation-pass').addClass('validation-fail').text(' Email address is not valid!');
+			}											
+		}
+		// If the email input text-box is empty then insert the "red X" indicator
+		else {
+			$('#email-flag').removeClass('fa fa-check').addClass('fa fa-times');
+			$('#email-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter email address!');				
+		}		
+	});
+
+	// Confirmed email address text input
+	$('#confirm-email').on('blur', function() {
+
+		// Confirmed email-address input value
+		var $confirmEmailInput = $(this).val();
+
+		// First email-address input value
+		var $emailInput = $('#email').val();
+
+		/* If the confirmed email input text-box is NOT empty 
+				- AND if the email is valid
+				- AND the two email-addresses entered do not match
+			(i.e. contains the necessary characters ('@', '.com,.org,etc')) then insert the "green tick" indicator
+		*/			
+		if($.trim($confirmEmailInput).length) {
+			if($emailRegex.test($confirmEmailInput)) {
+				if($confirmEmailInput === $emailInput) {
+					$('#confirm-email-flag').removeClass('fa fa-times').addClass('fa fa-check');
+					$('#confirm-email-validation').removeClass('validation-fail').addClass('validation-pass').text(' Email address is valid and matches!');
+				}
+				else { // If the two email-addresses do not match
+					$('#confirm-email-flag').removeClass('fa fa-check').addClass('fa fa-times');
+					$('#confirm-email-validation').removeClass('validation-pass').addClass('validation-fail').text(' Email addresses do not match!');	
+				}															
+			}
+			else { // If the confirmed email address DOES NOT contain necessary regex characters
+				$('#confirm-email-flag').removeClass('fa fa-check').addClass('fa fa-times');
+				$('#confirm-email-validation').removeClass('validation-pass').addClass('validation-fail').text(' Email address is not valid!');
+			}											
+		}
+		// If the confirmed email input text-box is empty then insert the "red X" indicator
+		else {
+			$('#confirm-email-flag').removeClass('fa fa-check').addClass('fa fa-times');
+			$('#confirm-email-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter email address!');				
+		}
+	});
+
+	// Contact message Regular Expression
+	var $messageRegex = new RegExp(/^[a-zA-Z0-9?$@#()'!,+\-=_:.&€£*%\s]+$/);
+
+	// Contact message text-area
+	$('#message').on('blur', function() {
+		// Message text input
+		var $messageInput = $(this).val();
+
+		/* If the message input text-box is NOT empty AND if the email is valid 
+			(i.e. contains the necessary characters) then insert the "green tick" indicator and message
+		*/
+		if($.trim($messageInput).length) {
+			if($messageRegex.test($messageInput)) {
+				$('#message-flag').removeClass('fa fa-times').addClass('fa fa-check');
+				$('#message-validation').removeClass('validation-fail').addClass('validation-pass').text(' Message is valid!');
+			}
+			else { // If any illegal characters are found in the contact message
+				$('#message-flag').removeClass('fa fa-check').addClass('fa fa-times');
+				$('#message-validation').removeClass('validation-pass').addClass('validation-fail').text(' Message is not valid!');
+			}			
+		}
+		// If the input text-box is NOT empty then insert the "green tick" indicator
+		else {
+			$('#message-flag').removeClass('fa fa-check').addClass('fa fa-times');
+			$('#message-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter message!');
+		}
+	});
+	
+	// Submit button functionality
+	$('#contact-form').on('submit', function() {
+		$('#send-button').on('click', function(event) {
+			event.preventDefault(); // Prevent the entire page from refreshing each time the form submits
+
+			// Flag to indicate that the "submit" was clicked
+			var $submitButtonClicked = $(this).data('clicked', true);
+
+			// If the "submit" button was clicked
+			if($submitButtonClicked) {
+				$('#contact-form').fadeOut('slow');
+			}
+		});		
+	});
+
+	// Clear all input fields when the user clicks the "reset" button
+	$('#reset-button').on('click', function(event) {
+		event.preventDefault(); // Prevent the entire page from reloading
+
+		// Flag to indicate that the "reset" was clicked
+		var $resetButtonClicked = $(this).data('clicked', true);
+
+		// Confirm dialogue box to ask the user to reset the form:
+		var confirmReset = confirm("Are you sure you want to reset and clear the form?");
+ 
+		/* If the reset button was clicked then confirm that the 
+			user wants to empty the fields  */
+		if($resetButtonClicked) {			
+			if(confirmReset) {
+				$('#first-name').val("");
+				$('#surname').val("");
+				$('#email').val("");
+				$('#message').val("");
+
+				// Flag all empty input fields appropriately
+				if($('#first-name').val("")) {
+					$('#first-name-flag').removeClass('fa fa-check').addClass('fa fa-times');
+					$('#first-name-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter first name!');
+				}
+				if($('#surname').val("")) {
+					$('#surname-flag').removeClass('fa fa-check').addClass('fa fa-times');
+					$('#surname-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter surname!');
+				}
+				if($('#email').val("")) {
+					$('#email-flag').removeClass('fa fa-check').addClass('fa fa-times');
+					$('#email-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter email address!');
+				}
+				if($('#confirm-email').val("")) {
+					$('#confirm-email-flag').removeClass('fa fa-check').addClass('fa fa-times');
+					$('#confirm-email-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter email address!');
+				}
+				if($('#message').val("")) {
+					$('#message-flag').removeClass('fa fa-check').addClass('fa fa-times');
+					$('#message-validation').removeClass('validation-pass').addClass('validation-fail').text(' Please enter message!');
+				}
+			}			
+		}			
+	});
+
+	// Hide the form when the user clicks the "close" button
+	$('#form-close-button').on('click', function(event) {
+		event.preventDefault(); // Prevent the entire from reloading
+
+		// Hide the form
+		$('#contact-form').fadeOut('slow');
+	});
+});
